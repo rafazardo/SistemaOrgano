be308ae839e61ce152cb639f4ba2cf6ee4715ad9
Rafael Zardo <rafael.zardo@outlook.com>
2023-03-30 22:27:59 -0300

    Criando ambiente de desenvolvimento

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+
+export function Banner() {
+    return (
+        <header className="banner" alt="imagem de banner">
+            <img src="./img/banner.png"></img>
+        </header>
+    )
+}
